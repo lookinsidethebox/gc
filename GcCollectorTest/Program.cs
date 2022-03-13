87c4f674a4ed27628f0d208437b1cb36fd1a0613
Анастасия С. Климова <klimovaas@pba.su>
2022-03-13 12:04:52 +0300

    initial

---
+++ GcCollectorTest/Program.cs
@@ -0,0 +1,20 @@
+﻿public class Program
+{
+	private static Random random = new Random();
+	private const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+	public static string RandomString(int length) =>
+		new string(Enumerable.Repeat(chars, length).Select(s => s[random.Next(s.Length)]).ToArray());
+
+	public static void Main()
+	{
+		var arr = new string[1000];
+
+		for (ulong i = 0; i < ulong.MaxValue; ++i)
+		{
+			var index = i % (ulong)arr.Length;
+			arr[index] = RandomString(85 * 1024);
+			arr[index] += "1";
+		}
+	}
+}
